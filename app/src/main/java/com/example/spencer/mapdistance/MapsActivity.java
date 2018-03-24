652b49419e718739d4165c12e405fc929cfd24e1
savaughn <savaughn@uark.edu>
2018-03-24 00:39:39 -0500

    manual drop pins now draggable

--- app/src/main/java/com/example/spencer/mapdistance/MapsActivity.java
+++ app/src/main/java/com/example/spencer/mapdistance/MapsActivity.java
@@ -39,6 +39,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Polygon area;
     Button clear;
     Button drop;
+    Button setMarker;
     Switch mode;
     private GoogleMap mMap;
 
@@ -46,13 +47,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Location location;
 
     final List<Marker> markers = new ArrayList<>();
+    final List<MarkerOptions> markerOptionsList = new ArrayList<>();
     final List<Double> distances = new ArrayList<>();
     final List<LatLng> coorList = new ArrayList<>();
 
     int count = -1;
     double totalArea = 1.0;
+    Boolean switchState = true;
 
-    public void handleMarkers(MarkerOptions markerOptions, Marker marker){
+    public void setMarkers(List<MarkerOptions> markerOptionsList, List<Marker> markers) {
+
+        for (int i = 0; i < markers.size(); i++) {
+            handleMarkers(markerOptionsList.get(i), markers.get(i));
+        }
+
+        //area = mMap.addPolygon(new PolygonOptions().add(markers.get(0).getPosition(), markers.get(1).getPosition(), markers.get(2).getPosition(),
+        //         markers.get(3).getPosition()).strokeColor(Color.BLACK));
+    }
+
+    public void handleMarkers(MarkerOptions markerOptions, Marker marker) {
 
         markers.add(marker);
         mPolyline = mMap.addPolyline(new PolylineOptions().geodesic(true));
@@ -64,7 +77,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         if (markers.size() > 1) {
-
             double distance = SphericalUtil.computeDistanceBetween(markers.get(count).getPosition(), markers.get(count + 1).getPosition());
             distances.add(distance);
             areaText.setText("Area (sq m): " + totalArea);
@@ -91,7 +103,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         count++;
     }
 
-    public double computeArea(List<Double> distances){
+    public double computeArea(List<Double> distances) {
         BigDecimal tmp = new BigDecimal(0);
         BigDecimal TWO = new BigDecimal(2);
 
@@ -100,13 +112,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         double c = distances.get(2);
         double d = distances.get(3);
 
-        for ( Double val : distances){
+        for (Double val : distances) {
             tmp = tmp.add(new BigDecimal(val.intValue()));
         }
 
         double s = tmp.divide(TWO).doubleValue();
-
-        return Math.sqrt((s-a)*(s-b)*(s-c)*(s-d));
+        return Math.sqrt((s - a) * (s - b) * (s - c) * (s - d));
     }
 
 
@@ -130,6 +141,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         lengthsText = findViewById(R.id.lengthsText);
         clear = findViewById(R.id.clearButton);
         drop = findViewById(R.id.drop);
+        setMarker = findViewById(R.id.setMarkerButton);
         mode = findViewById(R.id.modeSwitch);
         markerCountText.setText("Marker Count: " + markers.size());
         areaText.setText("Area (sq m): ");
@@ -144,8 +156,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             markerCountText.setText("no GPS permission");
         }
 
-        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()),18.0f));
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 18.0f));
 
         areaText.setTextColor(Color.RED);
         markerCountText.setTextColor(Color.RED);
@@ -154,66 +165,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mode.setTextColor(Color.BLACK);
         mode.setBackgroundColor(Color.LTGRAY);
 
-
-        mMap.setOnMapClickListener(new OnMapClickListener() {
-        int count = -1;
-        double totalArea = 1.0;
-        Boolean switchState = mode.isChecked();
-
-        @Override
-        public void onMapClick(LatLng latLng) {
-
-           clear.setOnClickListener(new View.OnClickListener() {
-               public void onClick(View v) {
-                   mMap.clear();
-                   markers.clear();
-                   markerCountText.setText("Marker Count: " + markers.size());
-                   areaText.setText("Area (sq m): ");
-                   lengthsText.setText(" ");
-                   coordinatesText.setText(" ");
-                   distances.clear();
-                   coorList.clear();
-                   count = -1;
-                   totalArea = 1;
-                   switchState = mode.isChecked();
-               }
-           });
-
-            drop.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    switchState = mode.isChecked();
-                    if (!switchState) {
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        try {
-                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        } catch (SecurityException e) {
-                            markerCountText.setText("no GPS permission");
-                        }
-                        LatLng newLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                        markerOptions.position(newLatLng);
-                        coorList.add(newLatLng);
-
-
-                        Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).draggable(true));
-                        handleMarkers(markerOptions, marker);
-                    }
-
+        mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mode.isChecked()) {
+                    drop.setVisibility(View.VISIBLE);
+                    setMarker.setVisibility(View.GONE);
+                } else {
+                    drop.setVisibility(View.GONE);
+                    setMarker.setVisibility(View.VISIBLE);
                 }
             }
-        );
-       switchState = mode.isChecked();
-       if(switchState) {
+        });
 
-           MarkerOptions markerOptions = new MarkerOptions();
-           markerOptions.position(latLng);
-           markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+        clear.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mMap.clear();
+                markers.clear();
+                markerCountText.setText("Marker Count: " + markers.size());
+                areaText.setText("Area (sq m): ");
+                lengthsText.setText(" ");
+                coordinatesText.setText(" ");
+                distances.clear();
+                coorList.clear();
+                count = -1;
+                totalArea = 1;
+                switchState = mode.isChecked();
+            }
+        });
 
-           coorList.add(latLng);
+        drop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (!mode.isChecked()) {
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    try {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    } catch (SecurityException e) {
+                        markerCountText.setText("no GPS permission");
+                    }
+                    LatLng newLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    markerOptions.position(newLatLng);
+                    coorList.add(newLatLng);
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).draggable(true));
+                    handleMarkers(markerOptions, marker);
+                }
 
-           Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latLng.latitude, latLng.longitude)).draggable(true));
-           handleMarkers(markerOptions, marker);
-        }
-       }
+            }
+        });
+
+
+
+            mMap.setOnMapClickListener(new OnMapClickListener() {
+                int count = 0;
+                Boolean button = false;
+                Marker marker = null;
+
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    if (mode.isChecked()) {
+                        final List<Marker> tmpMarkers = new ArrayList<>();
+                        final MarkerOptions markerOptions = new MarkerOptions();
+
+                        if(!button) {
+                            markerOptions.position(latLng);
+                            marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latLng.latitude, latLng.longitude)).draggable(true));
+                            button = true;
+                        }
+
+
+                        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+                            @Override
+                            public void onMarkerDragStart(Marker marker) {
+
+                            }
+
+                            @SuppressWarnings("unchecked")
+                            @Override
+                            public void onMarkerDragEnd(Marker arg0) {
+
+                                marker = mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getPosition().latitude, arg0.getPosition().longitude)).draggable(true));
+                                LatLng newLatLng = new LatLng(arg0.getPosition().latitude,arg0.getPosition().longitude);
+                                markerOptions.position(newLatLng);
+
+                            }
+
+                            @Override
+                            public void onMarkerDrag(Marker arg0) {
+                            }
+                        });
+
+                        //coorList.add(latLng);
+
+                        //Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latLng.latitude, latLng.longitude)).draggable(true));
+                        //handleMarkers(markerOptions, marker);
+                        setMarker.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View view) {
+                                count = 0;
+                                //setMarkers(markerOptionsList, tmpMarkers);
+                                tmpMarkers.add(marker);
+                                markerOptionsList.add(markerOptions);
+                                if(button)
+                                    handleMarkers(markerOptions,marker);
+                               button = false;
+                            }
+                        });
+                    }
+                    count++;
+                }
+            });
     }
-  );}
 }
